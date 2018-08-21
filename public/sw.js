1794399e4d9943e5f958d54aac4311e175b0cb9a
Fedde <feddevangils@gmail.com>
2018-08-21 12:35:13 +0200

    change sw.js;

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,18 @@
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('housepoints').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.php',
+        '/css/app.css',
+        '/js/app.js',
+        '/js/onsenui.js'
+      ]);
+    })
+  );
+ });
+
 self.addEventListener('push', function(e) {
 
 
