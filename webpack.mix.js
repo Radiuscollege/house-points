93d8adaf28c36903b52b8ebc763af12ccb8d6188
Fedde <feddevangils@gmail.com>
2018-08-21 13:01:43 +0200

    change webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -23,6 +23,19 @@ mix.webpackConfig({
       filename: 'sw.js',
       maximumFileSizeToCacheInBytes: 49194304,
       minify: false,
+
+      staticFileGlobs: ['public/**/*.{css,eot,svg,ttf,woff,woff2,js,html}'],
+        minify: true,
+        stripPrefix: 'public/',
+        handleFetch: true,
+        dynamicUrlToDependencies: { //you should add the path to your blade files here so they can be cached
+               //and have full support for offline first (example below)
+            '/': ['resources/views/base.blade.php', 'resources/views/pages/admin.blade.php', 'resources/views/pages/index.blade.php'],
+            // '/posts': ['resources/views/posts.blade.php']
+        },
+        staticFileGlobsIgnorePatterns: [/\.map$/, /mix-manifest\.json$/, /manifest\.json$/, /service-worker\.js$/],
+        navigateFallback: '/',
+
       runtimeCaching: [
         {
           handler: 'cacheFirst',
